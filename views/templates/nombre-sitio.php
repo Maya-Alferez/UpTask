db0546928086af70a85832b004a7ad4ec79844d2
Maya-Alferez <m.alferez17@gmail.com>
2022-09-06 11:57:36 -0500

    CSS crear cuenta
    Se termina de agregar las vistas con scss a la página de crear cuenta.

--- views/templates/nombre-sitio.php
+++ views/templates/nombre-sitio.php
@@ -1,2 +1,2 @@
 <h1 class="uptask">CatTrello</h1>
-<p class="tagline">Time tracking for better work, not overwork.</p>
+<p class="tagline">Time tracking for better work, simplify your productivity.</p>
