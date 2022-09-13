4ee817f5540bdd500af9ec1088e4721436f1e719
Maya-Alferez <m.alferez17@gmail.com>
2022-09-13 10:23:21 -0500

    Creando la vista de dashboard
    Añadir a la carpeta de templates los archivos de barra y sidebar para la página principal de dashboard

---
+++ views/templates/barra.php
@@ -0,0 +1,4 @@
+<div class="barra">
+    <p>Hi there! <span><?php echo $_SESSION['nombre']; ?></span></p>
+    <a href="/logout" class="cerrar-sesion">Log Out</a>
+</div>
---
+++ views/templates/sidebar.php
@@ -0,0 +1,9 @@
+<aside class="sidebar">
+    <h2>CatTrello</h2>
+
+    <nav class="sidebar-nav">
+        <a href="/dashboard">Projects</a>
+        <a href="/crear-proyecto">Create a new project</a>
+        <a href="/perfil">Profile</a>
+    </nav>
+</aside>
