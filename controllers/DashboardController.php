7fc43bcfa859bca99f1d9ab1bffd185ec7901bef
Maya-Alferez <m.alferez17@gmail.com>
2022-09-14 13:51:11 -0500

    Validación
    Validación para crear nuevos proyectos (nombre)

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -2,6 +2,7 @@
 namespace Controllers;
 
 use MVC\Router;
+use Model\Proyecto;
 
 class DashboardController {
     public static function index(Router $router) {
@@ -20,6 +21,18 @@ class DashboardController {
         session_start();
         isAuth();
         $alertas = [];
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $proyecto = new Proyecto($_POST);
+            
+            //Validación
+            $alertas = $proyecto->validarProyecto();
+
+            if(empty($alertas)) {
+                //Guardar proyecto
+                debuguear($proyecto);
+            }
+        }
 
         $router->render('dashboard/crear-proyecto', [
             'alertas' => $alertas,
