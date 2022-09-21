a474cf943ded455585b50c63434d1d30bb573c34
Maya-Alferez <m.alferez17@gmail.com>
2022-09-21 13:49:44 -0500

    Añadir etiquetas al proyecto actual
    Se crea Modelo de tarea para mostrar en la interfaz las etiquetas que vaya creando el usuario, así como la validación de que se haya creado.

--- src/js/tareas.js
+++ src/js/tareas.js
@@ -78,8 +78,39 @@
     }
 
     //Consultar el servidor para añadir una nueva tarea
-    function agregarTarea(tarea) {
+    async function agregarTarea(tarea) {
+        //Construir la petición
+        const datos = new FormData();
+        datos.append('nombre', tarea);
+        datos.append('proyectoId', obtenerProyecto());
 
+        try {
+            const url = 'http://localhost:3000/api/tarea';
+            const respuesta = await fetch(url, {
+                method: 'POST',
+                body: datos
+            });
+
+            const resultado = await respuesta.json();
+
+            mostrarAlerta(resultado.mensaje, resultado.tipo, document.querySelector('.formulario legend'));
+
+            if(resultado.tipo === 'exito') {
+                const modal = document.querySelector('.modal');
+                setTimeout(() => {
+                    modal.remove();
+                }, 2000);
+            }
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    function obtenerProyecto() {
+        const proyectoParams = new URLSearchParams(window.location.search);
+        const proyecto = Object.fromEntries(proyectoParams.entries());
+        return proyecto.id;
     }
 
 })();
