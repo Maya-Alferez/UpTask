f71b9b87afc21a3d459ff15781b2c7930b82c14a
Maya-Alferez <m.alferez17@gmail.com>
2022-10-06 15:44:54 -0500

    Eliminar etiqueta
    Eliminar la etiqueta que el usuario seleccione, tanto como en la vista, así como en base de datos.

--- src/js/tareas.js
+++ src/js/tareas.js
@@ -70,6 +70,9 @@
             btnEliminarTarea.classList.add('eliminar-tarea');
             btnEliminarTarea.dataset.idTarea = tarea.id;
             btnEliminarTarea.textContent = 'Delete';
+            btnEliminarTarea.ondblclick = function() {
+                confirmarEliminarTarea({...tarea});
+            }
 
             opcionesDiv.appendChild(btnEstadoTarea);
             opcionesDiv.appendChild(btnEliminarTarea);
@@ -222,9 +225,62 @@
             
             if(resultado.respuesta.tipo === 'exito') {
                 mostrarAlerta(resultado.respuesta.mensaje, resultado.respuesta.tipo, document.querySelector('.contenedor-nueva-tarea'));
+
+                tareas = tareas.map(tareaMemoria => {
+                    if(tareaMemoria.id === id) {
+                        tareaMemoria.estado = estado;
+                    }
+                    return tareaMemoria;
+                });
+
+                mostrarTareas();
             }
         } catch (error) {
             console.log(error);
+        }
+    }
+
+    function confirmarEliminarTarea(tarea) {
+        Swal.fire({
+            title: '¿Eliminar etiqueta?',
+            showCancelButton: true,
+            confirmButtonText: 'Sí',
+            cancelButtonText: 'No'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                eliminarTarea(tarea);
+            }
+        })
+    }
+
+    async function eliminarTarea(tarea) {
+        const {estado, id, nombre} = tarea;
+
+        const datos = new FormData();
+        datos.append('id', id);
+        datos.append('nombre', nombre);
+        datos.append('estado', estado);
+        datos.append('proyectoId', obtenerProyecto());
+
+        try {
+            const url = 'http://localhost:3000/api/tarea/eliminar';
+            const respuesta = await fetch(url, {
+                method: 'POST',
+                body: datos
+            });
+
+            const resultado = await respuesta.json();
+            if(resultado.resultado) {
+                //mostrarAlerta(resultado.mensaje, resultado.tipo, document.querySelector('.contenedor-nueva-tarea'));
+
+                Swal.fire('Eliminado', resultado.mensaje, 'success');
+
+                tareas = tareas.filter( tareaMemoria => tareaMemoria.id !== tarea.id);
+                mostrarTareas();
+            }
+
+        } catch (error) {
+
         }
     }
 
