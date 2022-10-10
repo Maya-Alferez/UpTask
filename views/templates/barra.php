7c19aca1fa713cb794a959282b1b94fe1f0cc389
Maya-Alferez <m.alferez17@gmail.com>
2022-10-10 17:51:40 -0500

    Menu mobile
    Se crean mediaquerys para el tamaño de celular, haciendo responsive la aplicación en celulares y tablets

--- views/templates/barra.php
+++ views/templates/barra.php
@@ -1,3 +1,11 @@
+<div class="barra-mobile">
+    <h1>CatTrello</h1>
+    <div class="menu">
+        <img src="build/img/menu.png" id="mobile-menu" alt="imagen-menu">
+    </div>
+</div>
+
+
 <div class="barra">
     <p>Hi there! <span><?php echo $_SESSION['nombre']; ?></span></p>
     <a href="/logout" class="cerrar-sesion">Log Out</a>
--- views/templates/sidebar.php
+++ views/templates/sidebar.php
@@ -1,9 +1,18 @@
 <aside class="sidebar">
-    <h2>CatTrello</h2>
-
+    <div class="contenedor-sidebar">
+        <h2>CatTrello</h2>
+        <div class="cerrar-menu">
+            <img src="build/img/cerrar.png" id="cerrar-menu" alt="imagen-cerrar-menu">
+        </div>
+    </div>
+    
     <nav class="sidebar-nav">
         <a class="<?php echo ($titulo === 'Projects') ? 'activo' : ''; ?>" href="/dashboard">Projects</a>
         <a class="<?php echo ($titulo === 'Create a new project') ? 'activo' : '';?>" href="/crear-proyecto">Create a new project</a>
         <a class="<?php echo ($titulo === 'Profile') ? 'activo' : '';?>" href="/perfil">Profile</a>
     </nav>
+
+    <div class="cerrar-sesion-mobile">
+        <a href="/logout" class="cerrar-sesion">Log out</a>
+    </div>
 </aside>
